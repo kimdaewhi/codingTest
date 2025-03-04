14f7df25efe234a1dd8f7d7b28738654ce174a0b
kimdaewhi <kimdaewhi@naver.com>
2025-03-05 08:16:49 +0900

    임시 커밋

--- Alghrithm/obj/Debug/net6.0/Alghrithm.AssemblyInfo.cs
+++ Alghrithm/obj/Debug/net6.0/Alghrithm.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Alghrithm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d934d3ef1dd2b52a5c8883d2d2acff4b57921ca1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Alghrithm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Alghrithm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
